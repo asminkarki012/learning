4e90bd55342b2cb999e64ac7c6d8b982c99276ea
asminkarki012 <asminkarki09@gmail.com>
2025-06-12 11:09:21 +0545

    approach learning pattern

--- learn-python/design-pattern/creational/builder.py
+++ learn-python/design-pattern/creational/builder.py
@@ -1,1 +1,4 @@
-# lets you construct complex object step by step,allowing you to produce different types and representations of an object.
+# What ? -> lets you construct complex object step by step,allowing you to produce different types and representations of an object.
+# Why ? ->
+# How ? ->
+
