defeacb60edc6932c602f0add82f66896e6a8c04
asminkarki012 <asminkarki09@gmail.com>
2025-06-14 22:29:05 +0545

    chore: builder pattern theory

--- learn-python/design-pattern/creational/builder.py
+++ learn-python/design-pattern/creational/builder.py
@@ -1,4 +1,14 @@
-# What ? -> lets you construct complex object step by step,allowing you to produce different types and representations of an object.
-# Why ? ->
-# How ? ->
+"""
+    What ?
+-> lets you construct complex object step by step,allowing you to produce different types and representations of an object.
 
+ Why is it needed?
+Problem:
+Creating complex objects with many parameters leads to long, confusing constructors, numerous overloads, and error-prone initialization.
+
+Solution:
+The Builder pattern separates object construction into clear, step-by-step methods in a builder class, allowing flexible, readable, and safe creation of complex objects without constructor overload.
+
+
+
+"""
